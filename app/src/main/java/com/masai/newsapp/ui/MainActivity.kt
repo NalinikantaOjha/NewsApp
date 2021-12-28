c8173aa270aa1a765d1c011b46aca4d1b82fe0f2
nalin <nalinikanta7978@gmail.com>
2021-12-28 14:10:23 +0530

    news APP

--- app/src/main/java/com/masai/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/masai/newsapp/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.masai.newsapp.ui
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -24,6 +25,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import android.net.NetworkInfo
+
+import android.net.ConnectivityManager
+
+
+
 
 class MainActivity : AppCompatActivity() ,OnClick{
     lateinit var adapter2: Adapter
@@ -46,14 +53,23 @@ class MainActivity : AppCompatActivity() ,OnClick{
         repository = DataRepository(userApi,dao)
         val Factory = ViewModelFactory(repository)
         viewModel2 = ViewModelProviders.of(this, Factory).get(MainViewModel::class.java)
-        loadApi()
-        setRecycle()
+       // loadApi()
+        if (isNetworkAvailable()){
+            loadApi()
+        }
+
         viewModel2.getData().observe(this, Observer {
-            miusicList.clear()
+
             miusicList.addAll(it)
-            adapter2.notifyDataSetChanged()
+            setRecycle()
         })
 
+    }
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
     private fun insertDataToDb(resultModels: List<Article>) {
         viewModel2.deleteDataFromDb()
@@ -92,6 +108,6 @@ class MainActivity : AppCompatActivity() ,OnClick{
         intent.putExtra("date",result.date)
         intent.putExtra("sorce",result.sorce)
         intent.putExtra("url",url)
-        startActivity(intent)
+       startActivity(intent)
     }
 }
